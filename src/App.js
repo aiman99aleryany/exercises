c5acb58f01cb0d835efbffb3dffdc1a52e38dd4e
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-17 02:51:25 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+const promptName = (name) => <h1>{name}</h1>;
+
 class App extends React.Component {
     render() {
-        return <div></div>;
+        return <div>{promptName('Aiman')}</div>;
     }
 }
 
