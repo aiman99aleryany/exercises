a4901b311b7ba07cc0191c47d3083c3b1c8887a3
Aiman Al Eryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-17 03:42:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Welcome></Welcome>
+                <Welcome name="Conor" age={30}></Welcome>
             </div>
         );
     }
