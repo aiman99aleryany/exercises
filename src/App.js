9f93fe4f5222a9dbe42bf47bc6f96285882ee11e
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-17 04:51:21 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Counter from './Counter';
 
 class App extends React.Component {
     render() {
-        return <div></div>;
+        return (
+            <div>
+                <Counter />
+            </div>
+        );
     }
 }
 
