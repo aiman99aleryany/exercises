47b9b81884b11e0892dc32e67aa74e04a10a2e59
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-17 03:33:21 +0200

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Welcome name="Aiman"></Welcome>
+                <Welcome></Welcome>
             </div>
         );
     }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// it doesn't show anything if I don't pass the name prop and that's because
+// React doesn't render 'undefined' or 'null' or 'false'
 class Welcome extends React.Component {
     render() {
         return (
@@ -9,5 +11,9 @@ class Welcome extends React.Component {
         );
     }
 }
+
+Welcome.defaultProps = {
+    name: 'New User',
+};
 
 export default Welcome;
