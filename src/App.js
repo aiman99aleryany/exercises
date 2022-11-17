607c58af8ef5cc08c767a698289247f06428cd5e
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-17 03:14:47 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Hello from './Hello';
 
 class App extends React.Component {
     render() {
-        return <div></div>;
+        return (
+            <div>
+                <Hello />
+            </div>
+        );
     }
 }
 
---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+                <p>What a beautiful Day!</p>
+            </div>
+        );
+    }
+}
+
+export default Hello;
