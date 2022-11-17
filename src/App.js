4b2a3eb9a2cef012166bbed49fdc1398adf2c07e
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-17 03:26:26 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import Welcome from './Welcome';
 class App extends React.Component {
     render() {
-        return <div></div>;
+        return (
+            <div>
+                <Welcome name="Aiman"></Welcome>
+            </div>
+        );
     }
 }
 
