f5908219e7d9c7d04ffce46de3f79210931a90fd
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-27 23:29:22 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import TodoList from './TodoList';
 class App extends React.Component {
     render() {
-        return <div></div>;
+        return <div>
+                <TodoList></TodoList>
+            </div>;
     }
 }
 
---
+++ src/TodoList.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+    state = {
+        newItem: '',
+        items: [],
+    };
+
+    handleInputChange = (event) => {
+        this.setState({
+            ...this.state,
+            newItem: event.target.value,
+        });
+    };
+
+    addnewItem = () => {
+        if(this.state.newItem === '')  return;
+        this.setState({
+            ...this.state,
+            items: [...this.state.items, this.state.newItem],
+            newItem: '',
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.state.items.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+                <input
+                    name="newItemField"
+                    type="text"
+                    value={this.state.newItem}
+                    onChange={this.handleInputChange}
+                />
+                <button onClick={this.addnewItem}>Add Item</button>
+            </div>
+        );
+    }
+}
+
+export default TodoList;
