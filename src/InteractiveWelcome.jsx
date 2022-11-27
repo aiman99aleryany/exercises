462a411caf2faa91f9439d7e85e8dec779901d78
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-27 10:41:03 +0200

    init

---
+++ src/InteractiveWelcome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+class InteractiveWelcome extends React.Component {
+    render() {
+        return <div>
+                
+            </div>;
+    }
+}
+
+
+export default InteractiveWelcome;
