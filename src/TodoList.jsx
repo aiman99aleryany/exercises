73c3e63f2b2ba1af4989f52ec3f93f36e97eae08
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-28 01:02:40 +0200

    done

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-
-
-// already done
 class TodoList extends React.Component {
     state = {
         newItem: '',
@@ -17,7 +14,7 @@ class TodoList extends React.Component {
     };
 
     addnewItem = () => {
-        if(this.state.newItem === '')  return;
+        if (this.state.newItem === '') return;
         this.setState({
             ...this.state,
             items: [...this.state.items, this.state.newItem],
@@ -25,20 +22,32 @@ class TodoList extends React.Component {
         });
     };
 
-
     resetAll = () => {
         this.setState({
-            ...this.setState,
+            ...this.state,
             items: [],
-        }) 
-    }
+        });
+    };
+
+    deleteItem = (index) => {
+        console.log(index);
+        this.setState((state) => {
+            const newState = state;
+            newState.items.splice(index, 1); // only slice the index 1 stands for 1 element including the index
+            console.log(newState.items);
+            return newState;
+        });
+    };
 
     render() {
         return (
             <div>
                 <ul>
                     {this.state.items.map((item, index) => (
-                        <li key={index}>{item}</li>
+                        <li key={index}>
+                            {item}
+                            <button onClick={() => this.deleteItem(index)}>x</button>
+                        </li>
                     ))}
                 </ul>
                 <input
