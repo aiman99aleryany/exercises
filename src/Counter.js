14a71377ef16afde64da513fb01c6761c262c1b3
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-27 09:29:28 +0200

    done

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import CounterDisplay from './CounterDisplay';
 
+// The constructor is not needed anymore in this case, because we have the componentDidMount method
+// and this method is executed after the Component has already rendered. and that's what we want.
+// to start rendering after the {state} has already rendered the first time with 0
 class Counter extends React.Component {
     state = {
         count: this.props.initValue,
     };
 
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         setInterval(() => {
             this.setState((state) => ({
                 count: state.count + this.props.incrementBy,
