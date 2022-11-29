89c9115c1cf2ebaf95fd9bb1f55afa570e31038f
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-29 20:35:36 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DisplayLanguage from './DisplayLanguage';
+import LanguageContext from './LanguageContext';
 
-class App extends React.Component {
-    render() {
-        return <div></div>;
+const INIT_STATE = {
+    language: 'EN',
+};
+
+function App() {
+    const [state, setState] = useState(INIT_STATE);
+
+    function handleLanguageSelection(event) {
+        setState({
+            ...state,
+            language: event.target.value,
+        });
     }
+
+    return (
+        <div>
+            <select value={state.language} onChange={handleLanguageSelection}>
+                <option value="EN">EN</option>
+                <option value="IT">IT</option>
+            </select>
+            <LanguageContext.Provider value={state.language}>
+                <DisplayLanguage></DisplayLanguage>
+            </LanguageContext.Provider>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/DisplayLanguage.jsx
@@ -0,0 +1,3 @@
+function DisplayLanguage() {}
+
+export default DisplayLanguage;
