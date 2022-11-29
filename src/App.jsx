c60f36c35ac3989f5277e10c13a1824e572ca611
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-11-29 20:50:00 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Weclome from './Welcome';
 
 class App extends React.Component {
     render() {
-        return <div></div>;
+        return (
+            <div>
+                <Weclome name="Aiman"></Weclome>
+            </div>
+        );
     }
 }
 
---
+++ src/Welcome.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Weclome = ({ name = 'World' }) => {
+    return <div>Hello, {name}</div>;
+};
+
+export default Weclome;
