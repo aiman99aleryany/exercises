bb1de9901d951cea77a79b7eb370eb813d774f3f
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-12-03 10:34:53 +0200

    done

--- src/useCounter.js
+++ src/useCounter.js
@@ -1,28 +1,15 @@
-import { useState } from "react";
-
+import { useCallback, useState } from 'react';
 
 const useCounter = (initValue = 0) => {
     const [counter, setCounter] = useState(initValue);
 
-    const incrementCounter = () => {
-        setCounter((counter) => counter + 1);
-    }
+    const incrementCounter = useCallback(() => setCounter((counter) => counter + 1), []);
 
-    const decrementCounter = () => {
-        setCounter((counter) => counter - 1);
-    }
+    const decrementCounter = useCallback(() => setCounter((counter) => counter - 1), []);
 
-    const resetCounter = () => {
-        setCounter(initValue);
-    }
+    const resetCounter = useCallback(() => setCounter(initValue), [initValue]);
 
-    return [
-        counter,
-        incrementCounter,
-        decrementCounter,
-        resetCounter
-    ];
-}
-
+    return [counter, incrementCounter, decrementCounter, resetCounter];
+};
 
 export default useCounter;
