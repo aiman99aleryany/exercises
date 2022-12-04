ffeebb883333de7390147df811b981d6d38545e7
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-12-04 04:45:01 +0200

    done

--- src/RouteRoot.jsx
+++ src/RouteRoot.jsx
@@ -1,38 +1,16 @@
-import React from "react";
-import { createBrowserRouter } from "react-router-dom";
-import { RouterProvider } from "react-router-dom";
-import App from "./App";
-import Route1 from './Route1';
-import Route2 from './Route2';
-import Route3 from './Route3';
-
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import App from './App';
+import Error from './Error';
 
 const indexRoute = {
     path: '/',
     element: <App />,
+    errorElement: <Error />,
 };
 
-const route1 = {
-    path: 'route1',
-    element: <Route1 />,
-};
-
-const route2 = {
-    path: 'route2',
-    element: <Route2 />,
-};
-
-const route3 = {
-    path: 'route3',
-    element: <Route3 />,
-};
-
-const router = createBrowserRouter([
-    indexRoute,
-    route1,
-    route2,
-    route3,
-]);
+const router = createBrowserRouter([indexRoute]);
 
 const RouteRoot = () => {
     return (
@@ -40,8 +18,6 @@ const RouteRoot = () => {
             <RouterProvider router={router}></RouterProvider>
         </React.StrictMode>
     );
-
 };
-
 
 export default RouteRoot;
